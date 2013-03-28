33e1b58f0361e116d1689bbe9936e55dd4e02e91
Daniel Casper <dancasper@gmail.com>
2013-03-28 09:26:50 -0600

    Add Setup File
    Including the setup tools script to create distributable zips.

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on Dec 24, 2012
+
+@author: daniel
+'''
+
+from distutils.core import setup
+
+
+setup(name = 'pyform',
+      version = '0.1a',
+      packages = ['util'],
+      py_modules = ['Form', 'Demos', 'Controls'])
+
+if __name__ == '__main__':
+  pass
