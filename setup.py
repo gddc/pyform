8d9f6f00baf8eca0af1661414d1efc5dfb5bb36e
Dani Casper <dancasper@gmail.com>
2021-11-29 08:59:52 -0500

    Update for Python3 compatibility. 4 space indentatation. Add requirements.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on Dec 24, 2012
+
+@author: daniel
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyform',
+    version='1.0.3',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+)
+
+if __name__ == '__main__':
+    pass
